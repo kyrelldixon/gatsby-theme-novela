549edbe310ac8807e268c60134177a7c28f110b1
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-03-06 20:44:06 -0600

    refactor: rename environment variables

--- functions/convertkit/convertkit.js
+++ functions/convertkit/convertkit.js
@@ -1,13 +1,13 @@
 const axios = require('axios')
 
 exports.handler = async (event) => {
-  const { FORM_ID, API_SECRET } = process.env
-  const URL = `https://api.convertkit.com/v3/forms/${FORM_ID}/subscribe`
+  const { CONVERTKIT_FORM_ID, CONVERTKIT_API_KEY } = process.env
+  const URL = `https://api.convertkit.com/v3/forms/${CONVERTKIT_FORM_ID}/subscribe`
 
   try {
     const body = {
       ...JSON.parse(event.body),
-      api_key: API_SECRET,
+      api_key: CONVERTKIT_API_KEY,
     }
 
     const { data, status } = await axios.post(URL, body)
