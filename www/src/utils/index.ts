44b871445421326b835b533830909715c7269c93
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-03-09 00:07:58 -0500

    refactor: move add to converkit function to utils

--- www/src/utils/index.ts
+++ www/src/utils/index.ts
@@ -18,3 +18,15 @@ export const scrollable = (action: string) => {
     document.body.style.height = '100%'
   }
 }
+
+export const addToConvertkit = (email) => {
+  const url = `/.netlify/functions/convertkit`
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email })
+  })
+    .then(res => res.json());
+}
